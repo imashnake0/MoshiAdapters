59fca5db216c7dc6b34beb32da735f077a52b196
imashnake0 <kamaleshpaluru07@gmail.com>
2022-11-04 13:06:30 -0400

    Created project

---
+++ app/src/main/java/com/imashnake/moshiadapter/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.imashnake.moshiadapter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.imashnake.moshiadapter.ui.theme.MoshiAdapterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoshiAdapterTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text("Android")
+                }
+            }
+        }
+    }
+}
